f40f2a0ef7dfecea5ec4a1553cb135a8188a4cd1
Andrew Mason <andrewmcodes@protonmail.com>
2020-12-17 00:10:50 -0500

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   purge: {
     enabled: process.env.NODE_ENV == "production",
     content: ['./src/**/*.{html,md,liquid}'],
+    options: {
+      safelist: ['bg-blue-300', 'bg-yellow-500', 'bg-green-300'],
+    }
   },
   darkMode: 'media', // 'media' or 'class'
   theme: {
@@ -71,61 +74,3 @@ module.exports = {
     require('tailwindcss-line-clamp'),
   ],
 }
-//   darkMode: false,
-//   theme: {
-//     fontFamily: {
-//       // display: ['Inter', 'system-ui', 'sans-serif'],
-//       // body: ['Inter', 'system-ui', 'sans-serif'],
-//       sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-//     },
-//     extend: {
-//       colors: {
-//         gray: colors.blueGray,
-//       },
-//     },
-//   },
-//   variants: {},
-//   plugins: [
-//     // require('@tailwindcss/forms'),
-//     require('@tailwindcss/typography'),
-//     // require('@tailwindcss/aspect-ratio'),
-//   ],
-// }
-
-/**
- * === Already Included Colors ===
- * gray: coolGray
- * red: red
- * yellow: amber
- * green: emerald
- * blue: blue
- * indigo: indigo
- * purple: violet
- * pink: pink
- */
-
-/**
- * === All Colors ===
- * blueGray
- * coolGray
- * gray
- * trueGray
- * warnGray
- * red
- * orange
- * amber
- * yellow
- * lime
- * green
- * emerald
- * teal
- * cyan
- * lightBlue
- * blue
- * indigo
- * violet
- * purple
- * fuchsia
- * pink
- * rose
- */
