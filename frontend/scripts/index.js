0298afe7a0b7d0542981096286af81ebd438208f
Andrew Mason <andrewmcodes@protonmail.com>
2021-02-16 00:55:09 -0500

    chore: fix stimulus (#18)

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,8 +1,10 @@
-import "alpine-turbo-drive-adapter"
-import "alpinejs"
+import { Application } from "stimulus"
 import * as Turbo from "@hotwired/turbo"
+import Reveal from "./controllers/reveal_controller"
 
 Turbo.setProgressBarDelay(1)
+const application = Application.start()
+application.register("reveal", Reveal)
 
 window.addEventListener("turbo:before-cache", () => {
   document.querySelectorAll(".prose h1, .prose h2, .prose h3, .prose h4").forEach((headerEl) => {
@@ -12,17 +14,3 @@ window.addEventListener("turbo:before-cache", () => {
     headerEl.appendChild(linkEl)
   })
 })
-
-// const env = document.querySelector("body").dataset.env
-
-// // Check that service workers are supported
-// if ("serviceWorker" in navigator && env === "production") {
-//   // use the window load event to keep the page load performant
-//   window.addEventListener("load", () => {
-//     try {
-//       navigator.serviceWorker.register("/sw.js")
-//     } catch (error) {
-//       console.error("Service worker registration failed: ", error)
-//     }
-//   })
-// }
