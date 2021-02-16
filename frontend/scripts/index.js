a13bb105938ca142b64233c164fec977550e6802
Andrew Mason <andrewmcodes@protonmail.com>
2021-02-15 23:36:32 -0500

    chore: fix turbo

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -4,7 +4,7 @@ import * as Turbo from "@hotwired/turbo"
 
 Turbo.setProgressBarDelay(1)
 
-window.addEventListener("turbo:load", (event) => {
+window.addEventListener("turbo:before-cache", () => {
   document.querySelectorAll(".prose h1, .prose h2, .prose h3, .prose h4").forEach((headerEl) => {
     const linkEl = document.createElement("a")
     linkEl.setAttribute("href", "#" + headerEl.id)
