b6acb18b6801186b05ca8650ce31584acb878143
Andrew Mason <andrewmcodes@protonmail.com>
2021-02-16 00:50:34 -0500

    chore: fix stimulus

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,8 +1,10 @@
-import "alpine-turbo-drive-adapter"
-import "alpinejs"
+import { Application } from "stimulus"
 import * as Turbo from "@hotwired/turbo"
+import Reveal from "./controllers/reveal_controller"
 
 Turbo.setProgressBarDelay(1)
+const application = Application.start()
+application.register("reveal", Reveal)
 
 window.addEventListener("turbo:before-cache", () => {
   document.querySelectorAll(".prose h1, .prose h2, .prose h3, .prose h4").forEach((headerEl) => {
