e2311d6b772611ef00cf1dd56ef9a29b93a5491e
Andrew Mason <andrewmcodes@protonmail.com>
2021-02-15 23:22:41 -0500

    chore: add subtitle to fix build

--- src/_components/page_banner_component.rb
+++ src/_components/page_banner_component.rb
@@ -1,12 +1,14 @@
 class PageBannerComponent < ApplicationComponent
   option :title, default: "TODO"
   option :description, default: false
+  option :subtitle, default: false
 
   def render_in(view_context)
     <<~HTML
       <div class="flex flex-col space-y-2 md:items-center md:text-center">
         <h1 class="text-4xl lg:text-6xl">#{title}</h1>
         #{description_markup}
+        #{subtitle_markup}
       </div>
     HTML
   end
@@ -15,5 +17,11 @@ class PageBannerComponent < ApplicationComponent
     return unless description
 
     "<p class=\"text-2xl\">#{description}</p>"
+  end
+
+  def subtitle_markup
+    return unless subtitle
+
+    "<p class=\"text-2xl\">#{subtitle}</p>"
   end
 end
