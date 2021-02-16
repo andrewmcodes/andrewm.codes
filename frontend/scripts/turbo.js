226b0a7246cb45bec9c6d90c7efa6a633f28fc0c
Andrew Mason <andrewmcodes@protonmail.com>
2021-02-15 21:00:32 -0500

    chore: add cors header

--- frontend/scripts/turbo.js
+++ frontend/scripts/turbo.js
@@ -1,7 +1,3 @@
 import * as Turbo from "@hotwired/turbo"
 
-Turbo.setProgressBarDelay(0)
-Turbo.start()
-document.addEventListener("turbolinks:before-cache", function () {
-  console.log('caching')
-})
+Turbo.setProgressBarDelay(1)
