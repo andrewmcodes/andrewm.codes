bc56b61b5a1b6427f6489cc151aaae0e86ad7beb
Andrew Mason <andrewmcodes@protonmail.com>
2021-02-17 00:20:28 -0500

    feat: add json feed

--- plugins/filters.rb
+++ plugins/filters.rb
@@ -2,6 +2,37 @@ module MyFilters
   def emoji(name, emoji)
     "<span role=\"img\" aria-label=\"#{name.capitalize}\">#{emoji}</span>"
   end
+
+  def title(page)
+    (page&.data&.title || site.metadata.title).strip + " | andrewm.codes"
+  end
+
+  def description(page)
+    page&.data&.description || site.metadata.description
+  end
+
+  def json_feed(page)
+    {
+      version: "https://jsonfeed.org/version/1",
+      title: site.metadata.title,
+      icon: absolute_url("/icon-192.png"),
+      home_page_url: absolute_url("/"),
+      feed_url: absolute_url("feed.json"),
+      items:
+        site
+          .posts
+          .docs
+          .map do |post|
+            {
+              id: post.url,
+              title: post.data.title,
+              content_html: post.content,
+              date_published: DateTime.rfc3339(post.data.published_at).to_s,
+              url: post.url
+            }
+          end
+    }.to_json
+  end
 end
 
 Liquid::Template.register_filter MyFilters
