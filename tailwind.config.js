a18fd3299543cb871b8b4b5f6d311e8118abc023
Andrew Mason <andrewmcodes@protonmail.com>
2021-02-25 22:29:35 -0500

    chore: fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,9 +81,9 @@ module.exports = {
   },
   plugins: [
     require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
-    require("tailwindcss-line-clamp"),
     require("tailwindcss-debug-screens"),
   ],
 }
