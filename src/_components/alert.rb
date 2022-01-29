7fb736b55777c237e0c6925fdf54b423773c99c6
Andrew Mason <andrewmcodes@protonmail.com>
2022-01-29 01:14:29 -0700

    chore: update components

--- src/_components/alert.rb
+++ src/_components/alert.rb
@@ -1,9 +1,12 @@
 class Alert < BoxComponent
   private
 
+  BASE_CLASSES = %(px-4 py-3 text-sm).freeze
+
   def classes
     class_list = opts[:classes]&.split(" ") || []
     class_list << level_classes(opts.fetch(:level, :info).to_sym)
+    class_list << BASE_CLASSES
 
     cleanup_keys :level
     class_list.flatten.join(" ")
@@ -12,13 +15,13 @@ class Alert < BoxComponent
   def level_classes(level)
     case level
     when :success
-      "px-4 py-3 text-sm text-green-700 bg-green-500 rounded-md dark:text-green-400 bg-opacity-10"
+      "text-green-700 bg-green-500 rounded-md dark:text-green-400 bg-opacity-10"
     when :warning
-      "px-4 py-3 text-sm text-yellow-700 bg-yellow-500 rounded-md dark:text-yellow-400 bg-opacity-10"
+      "text-yellow-700 bg-yellow-500 rounded-md dark:text-yellow-400 bg-opacity-10"
     when :error
-      "px-4 py-3 text-sm text-red-700 bg-red-500 rounded-md dark:text-red-400 bg-opacity-10"
+      "text-red-700 bg-red-500 rounded-md dark:text-red-400 bg-opacity-10"
     else
-      "px-4 py-3 text-sm text-sky-700 bg-sky-500 rounded-md dark:text-sky-400 bg-opacity-10"
+      "text-sky-700 bg-sky-500 rounded-md dark:text-sky-400 bg-opacity-10"
     end
   end
 end
