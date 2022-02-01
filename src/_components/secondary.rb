3fc04c4d66f1436c0edf819b6f4086916880aba5
Andrew Mason <andrewmcodes@protonmail.com>
2022-01-31 23:47:16 -0700

    chore: wip

--- src/_components/secondary.rb
+++ src/_components/secondary.rb
@@ -2,7 +2,7 @@ class Secondary < BoxComponent
   private
 
   def classes
-    "text-zinc-600 dark:text-zinc-400"
+    "text-zinc-600 dark:text-zinc-400 inline-block"
   end
 
   def remove_options
--- src/_components/stack.rb
+++ src/_components/stack.rb
@@ -2,7 +2,7 @@ class Stack < BoxComponent
   private
 
   def classes
-    [opts.fetch(:spacing, DEFAULT_SPACE_Y), DEFAULT_FLEX_COL]
+    [opts.fetch(:spacing, "space-y-4 md:space-y-8 lg:space-y-12"), DEFAULT_FLEX_COL]
   end
 
   def remove_options
