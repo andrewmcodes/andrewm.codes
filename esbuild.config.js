2c922599c70479aa21943b1f0243352488757b87
Andrew Mason <andrewmcodes@protonmail.com>
2022-02-07 18:32:20 -0700

    feat: add snippets

--- esbuild.config.js
+++ esbuild.config.js
@@ -8,19 +8,19 @@ const esbuildOptions = {}
 build(outputFolder, esbuildOptions)
 
 // WARNING: DO NOT COPY THIS FILE IT DOESN'T WORK CORRECTLY
-if (MODE === "development") {
-  const watcher = watch(["./src/**/*.*{html,md,erb,rb}", "./frontend/javascript/*.js*", "./frontend/styles/*.*css*"])
+// if (MODE === "development") {
+//   const watcher = watch(["./src/**/*.*{html,md,erb,rb}", "./frontend/javascript/*.js*", "./frontend/styles/*.*css*"])
 
-  watcher
-    .on("ready", () => {
-      console.log("esbuild: initial development build")
-    })
-    .on("change", path => {
-      console.log(`esbuild: file ${path} has been changed`)
-      build(outputFolder, esbuildOptions)
-    })
-    .on("raw", (event, path, details) => {
-      console.log("Raw event info:", event, path, details)
-    })
-  // watcher.close()
-}
+//   watcher
+//     .on("ready", () => {
+//       console.log("esbuild: initial development build")
+//     })
+//     .on("change", path => {
+//       console.log(`esbuild: file ${path} has been changed`)
+//       build(outputFolder, esbuildOptions)
+//     })
+//     .on("raw", (event, path, details) => {
+//       console.log("Raw event info:", event, path, details)
+//     })
+//   // watcher.close()
+// }
